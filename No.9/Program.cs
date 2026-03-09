dcb9fb8ba9221bc7bedd99ef287cfa0419dababb
Martinez . <pm789481@gmail.com>
2026-03-09 17:22:41 -0600

    Ingreso de datos

---
+++ No.9/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Ingrese el primer numero a sumar");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero a sumar");
+int n=int.Parse(Console.ReadLine());
