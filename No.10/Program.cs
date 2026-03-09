996b7fb721cc08bd9dfc505028c7c0fe042ad014
Martinez . <pm789481@gmail.com>
2026-03-09 17:32:41 -0600

    funcion

---
+++ No.10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace No._10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
--- No.9/Program.cs
+++ No.9/Program.cs
@@ -1,4 +1,10 @@
 ﻿Console.WriteLine("Ingrese el primer numero a sumar");
 int a=int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo numero a sumar");
-int n=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+int resultado=SumaMostrar(a, b);
+Console.WriteLine($"El resultado es: {resultado}");
+static int SumaMostrar(int a, int b)
+{
+    return a + b;
+}
