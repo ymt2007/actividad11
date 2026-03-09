8b893dfa38f530dffef469f2bd213c0f390abe5f
Martinez . <pm789481@gmail.com>
2026-03-09 17:19:02 -0600

    Cuadrado del numero con la funcion

--- No.8/Program.cs
+++ No.8/Program.cs
@@ -1,8 +1,14 @@
-﻿string op:
+﻿string op;
 do
 {
     Console.WriteLine("Ingrese el numero");
     int n=int.Parse(Console.ReadLine());
+    int resultado=MostrarCuadrado(n);
+    Console.WriteLine($"El resultado es {resultado}");
     Console.WriteLine("Desea ingresar otro numero? (s/n)");
     op=Console.ReadLine();
 } while (op != "n");
+static int MostrarCuadrado(int n)
+{
+    return n * n;
+}
