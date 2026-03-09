ba6589ebb24547d3115e1d7ee6f96d7d5a97f458
Martinez . <pm789481@gmail.com>
2026-03-09 16:50:23 -0600

    Mostrar menu con funcion

---
+++ No.5/Program.cs
@@ -0,0 +1,8 @@
+﻿void MostrarMenu()
+{
+    Console.WriteLine("Binevenido al menu, seleccione la opcion deseada");
+    Console.WriteLine("1. Sumar");
+    Console.WriteLine("2. Restar");
+    Console.WriteLine("3. Salir");
+}
+MostrarMenu();
