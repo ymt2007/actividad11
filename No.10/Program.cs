24eb03876969a8708102a240e8feb4a3be136b2b
Martinez . <pm789481@gmail.com>
2026-03-09 17:43:07 -0600

    Ingreso de datos

--- No.10/Program.cs
+++ No.10/Program.cs
@@ -1,10 +1,4 @@
-﻿namespace No._10
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿Console.WriteLine("Ingrese el texto que desea imprimir");
+string texto=Console.ReadLine();
+Console.WriteLine("Ingrese cuantas veces desea repetirlo");
+int veces=int.Parse(Console.ReadLine());
