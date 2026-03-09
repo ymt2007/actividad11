07ac1693ad821f55452e9f853587121a8d894122
Martinez . <pm789481@gmail.com>
2026-03-09 17:09:39 -0600

    Mostrar el doble

---
+++ No.7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ingrese el numero de cual desea ver el doble");
+int a=int.Parse(Console.ReadLine());
+int res=MostrarDoble(a);
+Console.WriteLine($"El resultado es: {res}");
+static int MostrarDoble(int a)
+{
+    return a*2;
+}
