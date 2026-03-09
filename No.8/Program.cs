82244c85c0e08e29d16038c0fb0ecdc5e26756e9
Martinez . <pm789481@gmail.com>
2026-03-09 17:14:40 -0600

    Ingreso de datos

---
+++ No.8/Program.cs
@@ -0,0 +1,8 @@
+﻿string op:
+do
+{
+    Console.WriteLine("Ingrese el numero");
+    int n=int.Parse(Console.ReadLine());
+    Console.WriteLine("Desea ingresar otro numero? (s/n)");
+    op=Console.ReadLine();
+} while (op != "n");
