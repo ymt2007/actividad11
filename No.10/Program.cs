9795da95c6f7c629e2f5d204522f3618638ea119
Martinez . <pm789481@gmail.com>
2026-03-09 17:50:40 -0600

    Imprimir el texto

--- No.10/Program.cs
+++ No.10/Program.cs
@@ -2,3 +2,11 @@
 string texto=Console.ReadLine();
 Console.WriteLine("Ingrese cuantas veces desea repetirlo");
 int veces=int.Parse(Console.ReadLine());
+ImprimirRepetido(texto, veces);
+static void ImprimirRepetido(string texto, int veces)
+{
+    for(int i=1;i<=veces;i++)
+    {
+        Console.WriteLine(texto);
+    }
+}
