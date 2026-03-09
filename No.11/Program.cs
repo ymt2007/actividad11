a2d8d1776815aa1be0f26c32744d09be131c4ba0
Martinez . <pm789481@gmail.com>
2026-03-09 17:59:59 -0600

    suma de numeros con return

---
+++ No.11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ingrese el primer numero a sumar");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero a sumar");
+int b=int.Parse(Console.ReadLine());
+int resultado = Sumar(a, b);
+Console.WriteLine($"El resultado es: {resultado}");
+static int Sumar(int a, int b)
+{
+    return a + b; 
+}
