01a9d4d4dc243a57a388b2e64214b1c38ee58fd3
Martinez . <pm789481@gmail.com>
2026-03-09 18:11:00 -0600

    Ingreso de datos

--- No.13/Program.cs
+++ No.13/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿Console.WriteLine("Ingrese el primer numero a multiplicar");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero a multiplicar");
+int b=int.Parse(Console.ReadLine());
