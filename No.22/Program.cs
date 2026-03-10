137a3812c93bfc7f067cebdd095301593987aa5f
Martinez . <pm789481@gmail.com>
2026-03-09 20:31:49 -0600

    ingreso de datos

---
+++ No.22/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Ingrese el texto que desea que se repita");
+string texto=Console.ReadLine();
+Console.WriteLine("Ingrese la cantidad de veces que desea que se repita");
+int veces=int.Parse(Console.ReadLine());
