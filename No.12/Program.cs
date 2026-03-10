52be5b98a7fb5f0302c1818ea633f8a08b7a7502
Martinez . <pm789481@gmail.com>
2026-03-09 18:08:46 -0600

    Funcion restar

--- No.12/Program.cs
+++ No.12/Program.cs
@@ -2,3 +2,9 @@
 int a=int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el sustraendo");
 int b=int.Parse(Console.ReadLine());
+int resultado=restar(a,b);
+Console.WriteLine($"El resultado es: {resultado}");
+static int restar(int a, int b)
+{
+    return a - b; 
+}
