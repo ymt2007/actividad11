c86f09e255f070a92beb593d85566348330e257f
Martinez . <pm789481@gmail.com>
2026-03-09 18:33:02 -0600

    verificacion

--- No.16/Program.cs
+++ No.16/Program.cs
@@ -1,2 +1,8 @@
 ﻿Console.WriteLine("Ingrese el numero a verificar");
 int n=int.Parse(Console.ReadLine());
+bool resultado= EsPositivo(n);
+Console.WriteLine(resultado);
+static bool EsPositivo(int n)
+{
+    return n > 0;
+}
