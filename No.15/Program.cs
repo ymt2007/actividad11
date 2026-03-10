242ab53f5da9577c6551321fb28df1c7f92b931e
Martinez . <pm789481@gmail.com>
2026-03-09 18:23:47 -0600

    verificacion

--- No.15/Program.cs
+++ No.15/Program.cs
@@ -1,2 +1,8 @@
 ﻿Console.WriteLine("Ingrese el numero a verificar");
 int n=int.Parse(Console.ReadLine());
+bool resultado = EsPar(n);
+Console.WriteLine(resultado);
+static bool EsPar(int n)
+{
+    return n % 2 == 0;
+}
