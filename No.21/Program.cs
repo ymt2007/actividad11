ad55ea401a1eca13d46ae7595cf8c2f69ece6f41
Martinez . <pm789481@gmail.com>
2026-03-09 20:28:41 -0600

    vocales en el texto

---
+++ No.21/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese el texto");
+string texto=Console.ReadLine();
+int resultado=ContarVocales(texto);
+Console.WriteLine($"El total de las vocales en las palabras es de: {resultado}");
+static int ContarVocales(string texto)
+{
+    int cont = 0;
+    for (int i = 0; i < texto.Length; i++)
+    {
+        if (texto[i] == 'a'|| texto[i] == 'e' || texto[i] == 'i' || texto[i] == 'o' || texto[i] == 'u')
+        {
+            cont++;
+        }
+    }
+    return cont;
+}
