30c05fb448ac29e60b591cffce4486201d0e7eaf
Martinez . <pm789481@gmail.com>
2026-03-09 18:17:25 -0600

    Funcion dividir

--- No.14/Program.cs
+++ No.14/Program.cs
@@ -2,3 +2,9 @@
 double a=double.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el divisor");
 double b=double.Parse(Console.ReadLine());
+double resultado = Dividir(a, b);
+Console.WriteLine($"El resultado es {resultado}");
+static double Dividir(double a, double b)
+{
+    return a / b;
+}
