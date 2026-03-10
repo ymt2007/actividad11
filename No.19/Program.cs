54e604252ee07927539dcdc460e8a0af0bd39db9
Martinez . <pm789481@gmail.com>
2026-03-09 19:01:16 -0600

    verificacion de multiplo

--- No.19/Program.cs
+++ No.19/Program.cs
@@ -2,3 +2,9 @@
 int n=int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo numero");
 int m=int.Parse(Console.ReadLine());
+bool resultado=EsMultiplo(n,m);
+Console.WriteLine($"El numero ingresado es multiplo? {resultado}");
+static bool EsMultiplo(int n, int m)
+{
+    return n % m == 0;
+}
