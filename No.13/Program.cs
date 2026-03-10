1199c95e3969991a92f5a9f641355eef78616ad5
Martinez . <pm789481@gmail.com>
2026-03-09 18:12:52 -0600

    Funcion multiplicar

--- No.13/Program.cs
+++ No.13/Program.cs
@@ -2,3 +2,9 @@
 int a=int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo numero a multiplicar");
 int b=int.Parse(Console.ReadLine());
+int resultado = Multiplicar(a, b);
+Console.WriteLine($"El resultado es: {resultado}");
+static int Multiplicar(int a, int b)
+{
+    return a * b;
+}
