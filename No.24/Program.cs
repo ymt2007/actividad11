a61fd75e42a854964580027cb63febc73179b315
Martinez . <pm789481@gmail.com>
2026-03-09 20:55:55 -0600

    Factorial

---
+++ No.24/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingrese el numero para factorial");
+int n=int.Parse(Console.ReadLine());
+int resultado = Factorial(n);
+Console.WriteLine($"El resultado del factorial es: {resultado}");
+static int Factorial(int n)
+{
+    int fac = 1;
+    for (int i = 1; i <=n; i++)
+    {
+        fac *= i;
+    }
+    return fac;
+}
