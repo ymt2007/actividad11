85af49f30e89c4bbe87bfd92b973eac2f6ef563a
Martinez . <pm789481@gmail.com>
2026-03-09 19:07:05 -0600

    contar letras

---
+++ No.20/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Ingrese una palabra");
+string texto=Console.ReadLine();
+int resultado=ContarLetras(texto);
+Console.WriteLine($"Hay {resultado} letras");
+static int ContarLetras(string texto)
+{
+    return texto.Count();
+}
