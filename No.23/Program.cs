ba3ce19d3d218d2d76cfd1fb9b043b9ea7d1e522
Martinez . <pm789481@gmail.com>
2026-03-09 20:47:50 -0600

    sumar de 1 a n

---
+++ No.23/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Ingrese el numero hasta donde desea sumar");
+int n=int.Parse(Console.ReadLine());
+int resultado = SunarDel1AlN(n);
+Console.WriteLine($"El resultado de la suma es: {resultado}");
+static int SunarDel1AlN(int n)
+{
+    int suma = 0;
+    for (int i = 0; i <= n; i++)
+    {
+        suma += i;
+    }
+    return suma;
+}
