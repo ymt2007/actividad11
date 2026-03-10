fc60685910a7d53adcba487479248e8520c6e22d
Martinez . <pm789481@gmail.com>
2026-03-09 20:39:01 -0600

    repeticion

--- No.22/Program.cs
+++ No.22/Program.cs
@@ -2,3 +2,14 @@
 string texto=Console.ReadLine();
 Console.WriteLine("Ingrese la cantidad de veces que desea que se repita");
 int veces=int.Parse(Console.ReadLine());
+string resultado = Repetir(texto, veces);
+Console.WriteLine(resultado);
+static string Repetir(string texto, int veces)
+{
+    string resultado = "";
+    for (int i = 0; i < veces; i++)
+    {
+        resultado = resultado + texto + "\n";
+    }
+    return resultado;
+}
