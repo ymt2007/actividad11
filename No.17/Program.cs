03b0dbc9a8dbb4f448c8314a686c60763723f404
Martinez . <pm789481@gmail.com>
2026-03-09 18:48:56 -0600

    identificacion del mayor

--- No.17/Program.cs
+++ No.17/Program.cs
@@ -2,3 +2,13 @@
 int a=int.Parse(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo numero");
 int b=int.Parse(Console.ReadLine());
+int resultado = Mayor(a, b);
+Console.WriteLine($"El mayor es: {resultado}");
+
+static int Mayor(int a, int b)
+{
+    if(a>b)
+        return a;
+    else 
+        return b;
+}
